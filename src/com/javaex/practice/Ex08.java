6f944a87860ce76c569d39f26245433c0a4a15c1
wonho <wonho951@gmail.com>
2021-05-31 21:18:21 +0900

    2021.05.31 배열 과제

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -6,15 +6,23 @@ public class Ex08 {
 		
 		// 문제 8
 		
-		int[] num = new int[6];
+		int[] num = new int[6];    // 방 6개 줌.
 		
 		for (int i = 0; i < num.length; i++) {
-			num[i] = (int)(Math.random()*45+1);
+			num[i] = (int)(Math.random()*45+1);       //num은 1~45까지의 랜덤값을 가짐.
+			
+			for (int j = 0; j<i; j++) {				
+				if (num[i] == num[j]) {					// 위의 i값과 j값을 비교했을때
+					i--;								// 같은 값이 나온다면 i값에서 1을 빼면  
+														// 다른값이 나올때 까지 계속 반복을 돌림.
+				}
+			}
 		}
+		for(int i = 0; i < num.length; i++)
+			System.out.print(num[i] + " ");
 		
-		for (int i = 0; i < num.length; i++) {
-			System.out.println(num[i]);
-		}
+				
+		
 		
 	}
 
